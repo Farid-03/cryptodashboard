ec91b32f6e65919ab2c186e922875e57d66a3f49
Farid Ibrahimzada <ibrahimzadef26@gmail.com>
2025-12-02 14:57:34 +0400

    configuration statistics to crypto

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,17 +1,42 @@
-import { Layout } from 'antd'
-
-
+import {Layout, Card, Statistic} from 'antd'
+import {ArrowDownOutlined, ArrowUpOutlined} from '@ant-design/icons';
 const siderStyle = {
-	textAlign: 'center',
-	lineHeight: '120px',
-	color: '#fff',
-	backgroundColor: '#1677ff',
+    padding: '1rem'
 }
 
-export default function AppSider () {
+export default function AppSider() {
     return (
         <Layout.Sider width='25%' style={siderStyle}>
-					Sider
-				</Layout.Sider>
+            <Card style={{
+                    marginBottom: '1rem'
+                }}>
+                <Statistic
+                    title="Active"
+                    value={11.28}
+                    precision={2}
+                    styles={{
+                        content: {
+                            color: '#3f8600'
+                        }
+                    }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"/>
+            </Card>
+
+            <Card>
+                <Statistic
+                    title="Idle"
+                    value={9.3}
+                    precision={2}
+                    styles={{
+                        content: {
+                            color: '#cf1322'
+                        }
+                    }}
+                    prefix={<ArrowDownOutlined />}
+                    suffix="%"/>
+            </Card>
+            Sider
+        </Layout.Sider>
     )
 }
